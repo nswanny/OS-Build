e6d57848de4511743d78aabe546a61e64bcbb58e
Nate Swanson <nateswanson213@gmail.com>
2022-04-17 15:23:10 -0500

    first commit

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,7 @@
+#include "print.h"
+
+void kernel_main() {
+    print_clear();
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("This is a test to see if the 64-bit kernel works!");
+}
